e69fd61e3d030787e310f918b0c8cd7043714067
Crystalflame <iicrystalflame@gmail.com>
2023-07-11 01:11:20 -0500

    finalize camlocation lints

--- src/MapLinter/Groups/CamLocations.lua
+++ src/MapLinter/Groups/CamLocations.lua
@@ -4,6 +4,17 @@ local IPS2DevKit = script.Parent.Parent.Parent
 
 local Types = require(IPS2DevKit.Types)
 
+local propertyChecks = {
+	"Color",
+	"Size",
+	"Transparency",
+	"Material",
+	"CanQuery",
+	"CanTouch",
+	"CanCollide",
+	"CastShadow",
+}
+
 return function(map: Folder): { Types.LintResultPartial }
 	local results = {}
 
@@ -28,7 +39,75 @@ return function(map: Folder): { Types.LintResultPartial }
 		end
 	end
 
-	-- TODO
+	-- Ensure there are at least 4 camera locations
+	local locations = camLocations:GetChildren()
+	if #locations < 4 then
+		table.insert(results, {
+			ok = false,
+			statusMessage = "Less than 4 camera locations.",
+		})
+	end
+
+	-- Ensure CamLocations are uniform
+	local template = IPS2DevKit.Assets.CamLocation
+	for i = 1, #locations do
+		local loc = camLocations:FindFirstChild(i)
+		if not loc then
+			table.insert(results, {
+				ok = false,
+				statusMessage = `Missing "{i}" camera location. Do you have stray instances?`,
+			})
+			break
+		end
+
+		local displayName = loc:GetAttribute("DisplayName")
+		if typeof(displayName) ~= "string" or string.len(displayName) <= 0 then
+			table.insert(results, {
+				ok = false,
+				statusMessage = `Camera location "{i}" has invalid "DisplayName" attribute.`,
+			})
+		end
+
+		for _, name in propertyChecks do
+			if loc[name] ~= template[name] then
+				table.insert(results, {
+					ok = false,
+					statusMessage = `Camera location "{i}" has invalid "{name}" property.`,
+				})
+			end
+		end
+
+		local faces = { Enum.NormalId.Top, Enum.NormalId.Bottom }
+		local dirTexture = template:FindFirstChildOfClass("Decal")
+		for _, child in loc:GetChildren() do
+			if not child:IsA("Decal") or child.Texture ~= dirTexture.Texture then
+				table.insert(results, {
+					ok = false,
+					statusMessage = `Camera location "{i}" has invalid children.`,
+				})
+				break
+			end
+
+			local index = table.find(faces, child.Face)
+			if not index then
+				table.insert(results, {
+					ok = false,
+					statusMessage = `Camera location "{i}" has invalid children.`,
+				})
+				break
+			end
+
+			table.remove(faces, index)
+		end
+
+		if #faces ~= 0 then
+			table.insert(results, {
+				ok = false,
+				statusMessage = `Camera location "{i}" has invalid children.`,
+			})
+			break
+		end
+	end
 
 	return results
 end
