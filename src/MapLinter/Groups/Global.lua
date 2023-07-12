3ec10712fdcd2afe00afca30821eb76657b3c894
Crystalflame <iicrystalflame@gmail.com>
2023-07-11 20:37:58 -0500

    remove unnecessary lint

--- src/MapLinter/Groups/Global.lua
+++ src/MapLinter/Groups/Global.lua
@@ -1,19 +1,9 @@
-local CollectionService = game:GetService("CollectionService")
-
 local IPS2DevKit = script.Parent.Parent.Parent
 
 local Types = require(IPS2DevKit.Types)
 
 return function(map: Folder): { Types.LintResultPartial }
 	local results = {}
-
-	-- Map shouldn't be tagged
-	if #CollectionService:GetTags(map) > 0 then
-		table.insert(results, {
-			ok = false,
-			statusMessage = "Map is tagged.",
-		})
-	end
 
 	-- No unachored BaseParts
 	for _, instance in map:GetDescendants() do
