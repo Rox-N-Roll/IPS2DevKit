017a89c0f65667ce3dca7e6a1fe62700b4ad1ec8
Crystalflame <iicrystalflame@gmail.com>
2023-07-13 21:27:05 -0500

    add VisProblems to Entrances lint group

--- src/MapLinter/Groups/Entrances.lua
+++ src/MapLinter/Groups/Entrances.lua
@@ -17,15 +17,15 @@ local allowedAttributes = {
 	BagVelocityMultiplier = "number",
 }
 
-local function isInvalidPath(path: Folder): boolean
+local function isInvalidPath(path: Folder): (boolean, Instance?)
 	for i = 1, #path:GetChildren() do
 		local node = path:FindFirstChild(i)
 		if not node or not node:IsA("Part") or node.CanCollide or node.CanQuery or node.CanTouch then
-			return true
+			return true, node
 		end
 	end
 
-	return false
+	return false, nil
 end
 
 return function(map: Folder): { Types.LintResultPartial }
@@ -56,10 +56,16 @@ return function(map: Folder): { Types.LintResultPartial }
 			local isInvalid, invalidName = Util.HasInvalidAttributes(instance, allowedAttributes)
 
 			if isInvalid then
-				table.insert(results, {
+				local res = {
 					ok = false,
 					statusMessage = `Entrance "{entrance.Name}" has instance "{instance:GetFullName()}" with invalid "{invalidName}" attribute.`,
-				})
+				}
+
+				if instance:IsA("BasePart") then
+					res.subject = instance
+				end
+
+				table.insert(results, res)
 				break
 			end
 		end
@@ -73,10 +79,16 @@ return function(map: Folder): { Types.LintResultPartial }
 			or node.CanCollide
 			or not CollectionService:HasTag(node, "EntranceNode")
 		then
-			table.insert(results, {
+			local res = {
 				ok = false,
 				statusMessage = `Entrance "{entrance.Name}" has invalid node.`,
-			})
+			}
+
+			if node:IsA("BasePart") then
+				res.subject = node
+			end
+
+			table.insert(results, res)
 		end
 
 		local seats = entrance:FindFirstChild("Seats")
@@ -87,6 +99,7 @@ return function(map: Folder): { Types.LintResultPartial }
 				table.insert(results, {
 					ok = false,
 					statusMessage = `Entrance "{entrance.Name}" node has excess attachments.`,
+					subject = node,
 				})
 			end
 
@@ -106,6 +119,7 @@ return function(map: Folder): { Types.LintResultPartial }
 						table.insert(results, {
 							ok = false,
 							statusMessage = `Entrance "{entrance.Name}" node is missing the "{i}" attachment.`,
+							subject = node,
 						})
 						break
 					end
@@ -130,11 +144,19 @@ return function(map: Folder): { Types.LintResultPartial }
 
 		local path = entrance:FindFirstChild("Path")
 		if path and path:IsA("Folder") then
-			if isInvalidPath(path) then
-				table.insert(results, {
+			local isInvalid, instance = isInvalidPath(path)
+
+			if isInvalid then
+				local res = {
 					ok = false,
 					statusMessage = `Entrance "{entrance.Name}" path has invalid children. Do you have stray instances or incorrect properties?`,
-				})
+				}
+
+				if instance:IsA("BasePart") then
+					res.subject = instance
+				end
+
+				table.insert(results, res)
 			end
 		else
 			table.insert(results, {
@@ -155,16 +177,24 @@ return function(map: Folder): { Types.LintResultPartial }
 						table.insert(results, {
 							ok = false,
 							statusMessage = `Entrance "{entrance.Name}" NPC path start has a missing seat assigned.`,
+							subject = spawnLoc,
 						})
 					end
 				end
 			end
 
-			if isInvalidPath(npcPath) then
-				table.insert(results, {
+			local isInvalid, instance = isInvalidPath(npcPath)
+			if isInvalid then
+				local res = {
 					ok = false,
 					statusMessage = `Entrance "{entrance.Name}" NPC path has invalid children. Do you have stray instances or incorrect properties?`,
-				})
+				}
+
+				if instance:IsA("BasePart") then
+					res.subject = instance
+				end
+
+				table.insert(results, res)
 			end
 		else
 			table.insert(results, {
