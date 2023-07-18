2dd284766fa003666805e26d5af664f6ec7f0218
Crystalflame <iicrystalflame@gmail.com>
2023-07-17 21:33:35 -0500

    update pathnode creation
    parent them smartly, name them sequentially

--- src/PanelCode/MapEntrances.lua
+++ src/PanelCode/MapEntrances.lua
@@ -23,10 +23,33 @@ function MapEntrances.CreatePathNodeAtNPC()
 		return
 	end
 
+	local pathHolder
+	local map = workspace:FindFirstChild("Map")
+	if map then
+		local entrances = map:FindFirstChild("Entrances")
+		if entrances then
+			for _, entrance in entrances:GetChildren() do
+				local path = entrance:FindFirstChild("Path")
+				local npcPath = entrance:FindFirstChild("NPCPath")
+
+				if path and npc.Parent == path then
+					pathHolder = path
+					break
+				elseif npcPath and npc.Parent == npcPath then
+					pathHolder = npcPath
+					break
+				end
+			end
+		end
+	end
+
+	-- Don't add one to number children because the NPC itself is parented there
+	local name = if pathHolder then #pathHolder:GetChildren() else "RENAME_ME"
+
 	local pathNode = assets.PathNode:Clone()
-	pathNode.Name = "RENAME_ME"
+	pathNode.Name = name
 	pathNode:PivotTo(hrp.CFrame)
-	pathNode.Parent = workspace
+	pathNode.Parent = pathHolder or workspace
 
 	ChangeHistoryService:SetWaypoint("Create Path Node at NPC")
 end
